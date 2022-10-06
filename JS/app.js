e94b83e2bfc67d86bfd2bc7667a88521eb845a4c
Todd Wolden <tcwolden@gmail.com>
2022-10-06 15:28:52 -0600

    made question four into a function

--- JS/app.js
+++ JS/app.js
@@ -51,21 +51,23 @@ function guessRunAway() {
 
 guessRunAway();
 
-let videoGame = prompt('Do I play video games?');
-
-if (videoGame === 'yes' || videoGame === 'y') {
-  // console.log('You\'re right, I do play video games.');
-  alert('YYou\'re right, I do play video games.');
-} else if (videoGame === 'no' || videoGame === 'n') {
-  // console.log('It was a fifty fifty chance, unfortunately you choose wrong.');
-  alert('It was a fifty fifty chance, unfortunately you choose wrong.');
-} else {
-  // console.log('Please answer with "yes" or "no."');
-  alert('Please answer with "yes" or "no."');
+function guessGame() {
+  let videoGame = prompt('Do I play video games?');
+  if (videoGame === 'yes' || videoGame === 'y') {
+    // console.log('You\'re right, I do play video games.');
+    alert('YYou\'re right, I do play video games.');
+  } else if (videoGame === 'no' || videoGame === 'n') {
+    // console.log('It was a fifty fifty chance, unfortunately you choose wrong.');
+    alert('It was a fifty fifty chance, unfortunately you choose wrong.');
+  } else {
+    // console.log('Please answer with "yes" or "no."');
+    alert('Please answer with "yes" or "no."');
+  }
 }
 
-let myJournal = prompt('Do I keep a journal?');
+guessGame();
 
+let myJournal = prompt('Do I keep a journal?');
 if (myJournal === 'no' || myJournal === 'n') {
   // console.log('You\'re right, I do not keep a journal.');
   alert('You\'re right ' + username + ', I do not keep a journal. Thanks for playing!');
